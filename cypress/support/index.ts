86b34f4c50489e8b369b105bc6de81a24184ad9a
vm31 <vmiddela@gmail.com>
2024-10-14 14:10:17 +0200

    screenshot link in test run

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,11 +1,16 @@
-// cypress/support/index.ts
-
 import './commands';
+import addContext from 'mochawesome/addContext'
 
 Cypress.on('uncaught:exception', (err: Error) => {
   console.error('Uncaught Exception:', err);
   if (err.message.includes('jQuery is not defined')) {
-      return false;
+    return false; // Ignore specific jQuery-related exceptions
   }
-  return true; 
+  return true; // Allow other exceptions to fail the test
 });
+
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    addContext({test}, { title: "Screenshot", value:`${Cypress.config('screenshotsFolder')}/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png` })
+  }
+})
